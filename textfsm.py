950b68684629b11dd920a93a4637191e7e47f29e
Daniel Harrison <harro@users.noreply.github.com>
2018-04-09 14:56:01 +1000

    Update textfsm.py

--- textfsm.py
+++ textfsm.py
@@ -28,7 +28,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 
 import getopt
 import inspect
